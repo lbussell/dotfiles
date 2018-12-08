9ac2f7ace0dd836ce86eb29ae66a8d945a536fbe
Logan Bussell <buswerks@charon.res.gatech.edu>
2018-12-07 22:26:42 -0500

    add python to install script

--- debian-install.sh
+++ debian-install.sh
@@ -5,6 +5,8 @@ apt -y uprade
 apt -y dist-upgrade
 
 # Basic utilities
+apt -y install dconf-cli
+apt -y install ssh
 apt -y install curl
 apt -y install git
 apt -y install yadm
@@ -12,10 +14,14 @@ apt -y install vim
 apt -y install pandoc
 apt -y install texlive-full
 
+# Programming
+apt -y install default-jdk
+apt -y install python-apt
+
 # Before running this script, enable contrib and non-free in /etc/apt/sources.lst
 # Nvidia graphics drivers
-dpkg --add-architecture i386
-apt install firmware-linux nvidia-driver nvidia-settings nvidia-xconfig
+# dpkg --add-architecture i386
+# apt install firmware-linux nvidia-driver nvidia-settings nvidia-xconfig
 
 # Misc
 apt -y install cowsay
