e5264c4777860f3132d12abba34d0b98409fd76c
Logan Bussell <loganbussell@microsoft.com>
2022-07-25 11:29:37 -0700

    Add script for creating smoke test prereqs

---
+++ scripts/create-prereqs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Use this in a directory with:
+# * x64 prereqs
+# * arm64 prereqs
+# * any extra nupkgs that you want to add
+
+set -euxo pipefail
+
+mkdir x64
+mkdir arm64
+
+tar -xzf *smoke*x64*.tar.gz -C x64/
+tar -xzf *smoke*arm64*.tar.gz -C arm64/
+
+# copy necessary arm64 packages
+cp arm64/*linux-arm64* x64/
+
+# copy extra packages
+cp *.nupkg x64/
+
+# create final prereqs tarball
+pushd x64
+    tar -czf ../dotnet-smoke-test-prereqs.tar.gz .
+popd
+
+# cleanup
+rm -rf x64/
+rm -rf arm64/
