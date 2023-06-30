e23df7915ff528e3e6e51589952c536bf149776a
Logan Bussell <loganbussell@microsoft.com>
2023-06-30 13:28:57 -0700

    Remove old content

--- scripts/go.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-cd /data/installer
-./build.sh /p:ArcadeBuildTarball=true /p:TarballDir=/data/tb/$1 /p:PreserveTarballGitFolders=true
-
-cd /data/tb/$1
-./prep.sh
-./build.sh --online
