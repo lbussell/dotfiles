da4dce08c2e45856e80e47720e1654aae70e1593
Logan Bussell <github@loganbussell.com>
2026-05-25 10:59:19 -0700

    Update tmux, nvim, and ghostty configs

--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Use terminal 256 colors only.
+-- Copied from https://github.com/LazyVim/LazyVim/discussions/1933
+vim.cmd.colorscheme = nil
+vim.opt.termguicolors = false
+vim.cmd("set t_Co=256")
