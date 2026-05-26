adcdebee5cfc3bc0865fd3cf053b6494946a76d5
Logan Bussell <loganbussell@microsoft.com>
2026-05-26 08:38:32 -0700

    Clear out colorschemes

--- home/dot_config/nvim/lua/plugins/colorscheme.lua
+++ home/dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,4 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
 
-return {
-  { "LazyVim/LazyVim", opts = { colorscheme = "color256" } },
-  { "rktjmp/lush.nvim" },
-  -- point Lazy at your local clone
-  { dir = vim.fn.expand("~/src/nvim-color256"), name = "color256", lazy = false, priority = 1000 },
-  {
-    "nvim-lualine/lualine.nvim",
-    opts = function(_, opts)
-      opts.options = opts.options or {}
-      opts.options.theme = require("color256.lualine")
-    end,
-  },
-}
+return {}
